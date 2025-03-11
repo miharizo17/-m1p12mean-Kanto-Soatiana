91f5f44e648cabc6b2e741cb150654e8276c34ec
miharizo17 <kantomiharizo@gmail.com>
2025-03-11 09:04:28 +0300

    test frontend

--- m1p12mean-Kanto-Soatiana/Garage_backend/backend/routes/emailRoutes.js
+++ m1p12mean-Kanto-Soatiana/Garage_backend/backend/routes/emailRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('../server');
 const nodemailer = require('nodemailer');
+
 router.post('/send-email', async (req, res) => {
     const { to, subject, text } = req.body;
 
